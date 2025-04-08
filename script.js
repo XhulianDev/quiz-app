56980906b903ad844808721a663837af4a653731
Xhulian <xhulianazemi9@gmail.com>
2025-04-08 19:42:26 +0200

    Versioni 8: Shto pyetje te tjera per fiziken ne JS

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function startPhysics() {
   const topicZone = document.getElementById("topic-zone");
   const questionZone = document.getElementById("question-zone");
+  const questionTitle = questionZone.querySelector("h2");
+  const questionText = questionZone.querySelector("p");
+  
+  questionTitle.textContent = "Pyetja 2";
+  questionText.textContent = "Kush e zbuloi gravitetin?";
+  
+  questionTitle.textContent = "Pyetja 3";
+  questionText.textContent = "Cfare matet me m/s²?";
   
   const button1 = document.getElementById("b1");
   const answer1 = button1.dataset.answer;
