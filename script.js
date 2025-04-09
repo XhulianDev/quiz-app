a52b9dcabb7b088559986d1fcd69b9360d09099b
Xhulian <xhulianazemi9@gmail.com>
2025-04-09 20:26:58 +0200

    Versioni 11: Shto fushen correct me pergjigjet e sakta per 6 pyetje

--- script.js
+++ script.js
@@ -6,14 +6,19 @@ function showQuestion(index) {
   const questionText = questionZone.querySelector("p");
   const button1 = document.getElementById("b1");
   const button2 = document.getElementById("b2");
+  const correctAnswer = document.getElementById("answer");
 
   const physicsQuestions = [
-    { title: "Pyetja 1", text: "Çfarë është F = ma?", answers: ["Ligji i Njutonit", "Ekuacioni i Ajnshtajnit"] },
-    { title: "Pyetja 2", text: "Kush e zbuloi gravitetin?", answers: ["Njutoni", "Ajnshtajni"] },
-    { title: "Pyetja 3", text: "Çfarë matet me m/s²?", answers: ["Nxitimi", "Shpejtësia"] },
-    { title: "Pyetja 4", text: "Cila eshte shpejtesia e drites ne vakum?", answers: ["300,000 km/s", "150,000 km/s"] },
-    { title: "Pyetja 5", text: "Cfare eshte energjia potenciale?", answers: ["Energji nga lëvizja", "Energji nga pozicioni"] },
-    { title: "Pyetja 6", text: "Kush e zhvilloi teorinë e relativitetit?", answers: ["Ajnshtajni", "Galileu"] }
+    { title: "Pyetja 1", text: "Çfarë është F = ma?", answers: ["Ligji i Njutonit", "Ekuacioni i Ajnshtajnit"], correct: 
+    ["Ekuacioni i Ajnshtajnit"] },
+    { title: "Pyetja 2", text: "Kush e zbuloi gravitetin?", answers: ["Njutoni", "Ajnshtajni"], correct: "Njutoni" },
+    { title: "Pyetja 3", text: "Çfarë matet me m/s²?", answers: ["Nxitimi", "Shpejtësia"], correct: "Nxitimi" },
+    { title: "Pyetja 4", text: "Cila eshte shpejtesia e drites ne vakum?", answers: ["300,000 km/s", "150,000 km/s"], correct: 
+    "300,000 km/s" },
+    { title: "Pyetja 5", text: "Cfare eshte energjia potenciale?", answers: ["Energji nga lëvizja", "Energji nga pozicioni"], 
+    correct: "Energji nga pozicioni" },
+    { title: "Pyetja 6", text: "Kush e zhvilloi teorinë e relativitetit?", answers: ["Ajnshtajni", "Galileu"], correct: 
+    "Ajnshtajni" }
   ];
 
   questionTitle.textContent = physicsQuestions[index].title;
@@ -22,6 +27,7 @@ function showQuestion(index) {
   button2.textContent = physicsQuestions[index].answers[1];
   button1.dataset.answer = physicsQuestions[index].answers[0];
   button2.dataset.answer = physicsQuestions[index].answers[1];
+  
 }
 
 function startPhysics() {
@@ -29,6 +35,7 @@ function startPhysics() {
   const questionZone = document.getElementById("question-zone");
   const button1 = document.getElementById("b1");
   const button2 = document.getElementById("b2");
+  const correctAnswer = document.getElementById("answer");
 
   topicZone.style.display = "none";
   questionZone.style.display = "block";
@@ -48,4 +55,10 @@ function startPhysics() {
       showQuestion(currentQuestion);
     }
   });
+  
+  if (button1.dataset.answer === physicsQuestions[i].correct) {
+    correctAnswer.textContent = "Sakte!";
+  } else {
+    correctAnswer.textContent = "Gabim!";
+  }
 }
